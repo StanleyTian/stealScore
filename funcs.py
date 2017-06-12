f65bff31f6879d12fe4adb0b1eb02910bd9fcc76
史丹利复合田 <grasslight@qq.com>
2017-06-12 12:21:09 +0800

    完成单个帖子所有页面的解析，并合并为一个Markdown文件

--- funcs.py
+++ funcs.py
@@ -1,4 +1,5 @@
 import urllib.request
+import requests
 from bs4 import BeautifulSoup
 import re
 
@@ -9,17 +10,18 @@ def power(x):
 def crawlSinglePage(url):
     # url = "http://bbs.guitarera.com/thread-2049-1-1.html"
     mdContent = ""
-    req = urllib.request.Request(url, headers={
+
+    headers = {
         'Connection': 'Keep-Alive',
         'Accept': 'text/html, application/xhtml+xml, */*',
         'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
-    })
-    data = urllib.request.urlopen(req).read()
-    htmlContent = data.decode('gbk');# 吉他时代论坛是gbk编码
-
+    }
+    r = requests.get(url,headers = headers)
+    htmlContent = r.text
+    #print(r.text)
     soup = BeautifulSoup(htmlContent);
-    print(soup.title.string)
+    #print(soup.title.string)
     postSubject = soup.select("#thread_subject")[0].string # select返回是一个list，即使只有一个元素也是，[0]表示第一个元素
     mdContent += postSubject+"\r\r"
 
@@ -56,17 +58,52 @@ def formatSingleFloor(id,soup):
     if len(postDownloadList) > 0 :
         for singleLink in postDownloadList:
             downloadLinks[singleLink.string] = singleLink.attrs['href'];
-    print(id);
-    print(postTitle)
-    print(postContent)
+    #print(id);
+    #print(postTitle)
+    #print(postContent)
 
     fullContent += "## "+postTitle + "\r\r"\
                    + postContent + "\r\r"\
                    + "##### 曲谱下载链接："+'\r\r'
 
     for i in downloadLinks:
-        fullContent += "[" + i +"](" + downloadLinks[i] + ")" + "\r\r"
+        fullContent += "[" + i +"](http://bbs.guitarera.com/" + downloadLinks[i] + ")" + "\r\r"
 
     fullContent += "--------------------------" + "\r\r"
 
     return fullContent
+
+def extractAllNumbers(str):
+    m = re.findall('(\w*[0-9]+)\w*', str)
+    return m
+
+def getPostAllPagesCountAndPageName(url):
+    headers = {
+        'Connection': 'Keep-Alive',
+        'Accept': 'text/html, application/xhtml+xml, */*',
+        'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
+    }
+    r = requests.get(url, headers=headers)
+    htmlContent = r.text
+    soup = BeautifulSoup(htmlContent)
+    countInfo = soup.select("#pgt .pg label span")[0].text
+    postSubject = soup.select("#thread_subject")[0].string # select返回是一个list，即使只有一个元素也是，[0]表示第一个元素
+
+    count = int(extractAllNumbers(countInfo)[0])
+    return [count,postSubject]
+
+def getPostAllPagesUrl(baseUrl,totalPageCount):
+    urlLength = len(baseUrl)
+    allUrls = []
+    a = baseUrl.rfind('-',0,urlLength) # 反向查找第一个'-'
+    b = baseUrl.rfind('-',0,a)          # 反向查找第二个'-'
+
+    #print(baseUrl[b+1:a])
+
+    part1 = baseUrl[0:b+1]
+    part2 = baseUrl[a:urlLength]
+
+    for i in range(totalPageCount):
+        allUrls.append(part1+str(i+1)+part2)
+    return allUrls
--- getScore.py
+++ getScore.py
@@ -1,6 +1,7 @@
 #encoding:UTF-8
-import urllib.parse
-import urllib.request
+import requests
+import re
+import os
 import funcs
 
 # http://bbs.guitarera.com/thread-2049-1-1.html # 李斯特页面
@@ -10,25 +11,23 @@ import funcs
 # url_values = urllib.parse.urlencode(data)
 url = "http://bbs.guitarera.com/thread-2049-1-1.html"
 
-content = funcs.crawlSinglePage(url);
-# req = urllib.request.Request(url, headers = {
-#     'Connection': 'Keep-Alive',
-#     'Accept': 'text/html, application/xhtml+xml, */*',
-#     'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko'
-# })
-# data = urllib.request.urlopen(req).read()
-# data = data.decode('gbk')
-#
-#
-#
-#
-# print(data)
-
-# 打开文件
-fo = open("test.md", "w+")
-print ("文件名: ", fo.name)
-
-line = fo.write(content)
-
+# preprocess 获取总页数
+# test(url)
+[totalCount,pageName] = funcs.getPostAllPagesCountAndPageName(url)
+# 依据pageName新建一个文件夹（如果不存在的话）
+scoreFolderPath = "./score"
+if not os.path.exists(scoreFolderPath):
+    os.makedirs(scoreFolderPath)
+# 获取当前帖子的所有页面，即 1,2,3,...,7 (共7页）
+allUrls = funcs.getPostAllPagesUrl(url,totalCount)
+# print(allUrls)
+allContent = ""
+for singleUrl in allUrls:
+    content = funcs.crawlSinglePage(singleUrl) #分别对每一个
+    print("完成了一页帖子的输出，长度为",len(content),'url:',singleUrl)
+    allContent +=content
+    # 打开文件
+fo = open(scoreFolderPath+"/"+pageName+".md", "w+")
+#print ("文件名: ", fo.name)
+line = fo.write(allContent)
 fo.close()
